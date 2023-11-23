2ce05b7fc21595dc831c5c657e8a5956596446f3
Ian Chua <ianchuajl@hotmail.com>
2023-11-23 20:19:33 +1300

    about page done for now for small media

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,47 @@
 export default function About() {
   return (
-    <section>
-      
+    <section className="py-8 px-4 dark:text-amber-50">
+      <h2 className="pt-2 pb-8 text-center text-2xl font-bold">
+        About GW Lawyers Ltd
+      </h2>
+      <p className="pb-10 leading-7">
+        GW Lawyers Limited is a humble law firm based in New Lynn. We are
+        incorporated under the Lawyers and Conveyancer Act 2006 and the
+        Companies Act 1993. We aim to provide <b>low price</b> and{' '}
+        <b>high quality</b>
+        Legal services.
+      </p>
+      <p>Our legal services include:</p>
+      <div className="px-4 pt-2 pb-10">
+        <ul className="list-inside list-disc leading-9">
+          <li>
+            Property Law &#40;Purchase and Sale of Residential and Commercial
+            Property; Power of Attorney; Refinance; Subdivision etc&#41;
+          </li>
+          <li>
+            Commercial Law &#40;Purchase and Sale of Business; Commercial Lease,
+            Franchise; Loan Agreement; General Security Agreement etc&#41;
+          </li>
+          <li>Estate Planning, Trusts, and Wills</li>
+          <li>
+            Company Law &#40;Purchase and Sale of Shares; Incorporation;
+            Shareholders Agreement; Constitutions etc&#41;
+          </li>
+          <li>Relationship Property</li>
+          <li>Immigration Law</li>
+          <li>Debt Recovery and Civil Litigation</li>
+          <li>
+            Employment Law &#40;Employment Agreement; Employment Disputes
+            etc&#41;
+          </li>
+        </ul>
+      </div>
+      <p className="leading-7 pb-10">
+        We act in the best interest of our clients and value the relationship
+        between our clients and us. We respect our clients and understand
+        wholeheartedly that it is our clients who provide us with this
+        opportunity to serve them.
+      </p>
     </section>
   )
 }
--- src/app/components/MainSection.tsx
+++ src/app/components/MainSection.tsx
@@ -6,7 +6,7 @@ export default function MainSection() {
     <section className="w-full grid grid-row-2 md:grid-cols-2 lg:grid-cols-3">
       <div className="flex flex-col justify-center text-center p-4 dark:bg-gradient-to-b from-amber-950 to-stone-950 md:p-2 dark:text-amber-50">
         <h2 className="py-8 text-2xl font-bold md:pt-0 ">
-          Qualified And Confidential{' '}
+          Qualified And Confidential
           <span className="whitespace-nowrap">Legal Advice</span>
         </h2>
         <p className="pb-8 ">
@@ -18,8 +18,6 @@ export default function MainSection() {
         <Image
           className="mt-0 w-full h-min"
           src={mainImg}
-          // width={1800}
-          height={200}
           alt="Books on the table"
           priority={true}
         />
