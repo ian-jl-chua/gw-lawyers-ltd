a2c6c614f4ea8f3c95220a53bfed279b1af30956
Ian Chua <ianchuajl@hotmail.com>
2023-11-23 20:27:40 +1300

    about page done for now for medium and large devices

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,18 +1,16 @@
 export default function About() {
   return (
-    <section className="py-8 px-4 dark:text-amber-50">
+    <section className="py-8 px-4 dark:text-amber-50 lg:px-8">
       <h2 className="pt-2 pb-8 text-center text-2xl font-bold">
         About GW Lawyers Ltd
       </h2>
-      <p className="pb-10 leading-7">
+      <p className="pb-10 leading-7 md:px-8">
         GW Lawyers Limited is a humble law firm based in New Lynn. We are
         incorporated under the Lawyers and Conveyancer Act 2006 and the
-        Companies Act 1993. We aim to provide <b>low price</b> and{' '}
-        <b>high quality</b>
-        Legal services.
+        Companies Act 1993. We aim to provide <b>low price</b> and <b>high quality</b> Legal services.
       </p>
-      <p>Our legal services include:</p>
-      <div className="px-4 pt-2 pb-10">
+      <p className="md:px-8">Our legal services include:</p>
+      <div className="px-4 pt-2 pb-10 md:px-12">
         <ul className="list-inside list-disc leading-9">
           <li>
             Property Law &#40;Purchase and Sale of Residential and Commercial
@@ -36,7 +34,7 @@ export default function About() {
           </li>
         </ul>
       </div>
-      <p className="leading-7 pb-10">
+      <p className="leading-7 pb-10 md:px-8">
         We act in the best interest of our clients and value the relationship
         between our clients and us. We respect our clients and understand
         wholeheartedly that it is our clients who provide us with this
