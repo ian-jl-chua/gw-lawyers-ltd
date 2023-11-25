aaaf60d70d85c098bf7bfbfabcf611bb8339402d
Ian Chua <ianchuajl@hotmail.com>
2023-11-25 21:19:43 +1300

    footer is done for now

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,17 +3,17 @@ import Link from 'next/link'
 export default function Footer() {
   const thisYear = new Date()
   return (
-    <footer className="p-4 bg-amber-100/80 dark:bg-stone-950">
-      <div className="grid pb-4">
+    <footer className="p-4 bg-amber-100/80 dark:bg-stone-900 md:p-8">
+      <div className="block pb-10">
         <div>
-          <Link className="font-bold text-amber-900 dark:text-amber-100" href="/">
+          <Link className="text-3xl font-bold text-amber-900 dark:text-amber-100" href="/">
             GW LAWYERS LTD
           </Link>
           <p>NZBN &#58; 9429031418959</p>
         </div>
         <div></div>
       </div>
-      <p className="text-xs dark:text-amber-100/60">
+      <p className="text-xs text-amber-900/50 dark:text-amber-100/50">
         &copy; {thisYear.getFullYear()} GW Lawyers Ltd. All rights reserved.
       </p>
     </footer>
