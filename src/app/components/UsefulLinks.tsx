253b121d877bd3b960b1d5c8a0eae43b75e05f50
Ian Chua <ianchuajl@hotmail.com>
2023-11-29 22:09:15 +1300

    fixed links

--- src/app/components/UsefulLinks.tsx
+++ src/app/components/UsefulLinks.tsx
@@ -5,27 +5,27 @@ export default function UsefulLinks() {
     {
       id: 1,
       linkName: 'Inland Revenue',
-      linkRef: 'https://www.ird.govt.nz/',
+      linkRef: 'https://www.ird.govt.nz',
     },
     {
       id: 2,
       linkName: 'Immigration New Zealand',
-      linkRef: 'https://www.ird.govt.nz/',
+      linkRef: 'https://www.immigration.govt.nz',
     },
     {
       id: 3,
       linkName: 'New Zealand Companies Register',
-      linkRef: 'https://www.ird.govt.nz/',
+      linkRef: 'https://www.companiesoffice.govt.nz',
     },
     {
       id: 4,
       linkName: 'New Zealand Law Society',
-      linkRef: 'https://www.ird.govt.nz/',
+      linkRef: 'https://www.lawsociety.org.nz',
     },
     {
       id: 5,
       linkName: 'New Zealand Legislation',
-      linkRef: 'https://www.ird.govt.nz/',
+      linkRef: 'https://www.legislation.govt.nz',
     },
   ]
   return (
