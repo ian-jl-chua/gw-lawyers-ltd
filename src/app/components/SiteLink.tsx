37db1754e9cc89f5ea554e7724c15910b76dff0f
Ian Chua <ianchuajl@hotmail.com>
2023-12-03 21:23:05 +1300

    a link is more sutable for SiteLink file

--- src/app/components/SiteLink.tsx
+++ src/app/components/SiteLink.tsx
@@ -1,5 +1,3 @@
-import Link from 'next/link'
-
 interface Link {
   id: number
   linkName: string
@@ -15,13 +13,13 @@ export default function SiteLink({ links }: SiteLinkProps) {
     <ul className="px-4 py-4 text-center list-inside leading-10 sm:text-xl lg:px-8 lg:text-left lg:text-2xl">
       {links.map((link) => (
         <li key={link.id} className="sm:py-4">
-          <Link
+          <a
             href={link.linkRef}
-            className="underline-offset-4 hover:text-amber-600 hover:underline hover:decoration-2 hover:underline-offset-8 "
-            target="blank"
+            className=" hover:text-amber-600 hover:underline hover:decoration-2 hover:underline-offset-8 "
+            target="_blank" rel="noopener noreferrer"
           >
             {link.linkName}
-          </Link>
+          </a>
         </li>
       ))}
     </ul>
--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -8,7 +8,7 @@ export default function Team() {
     <section className="section">
       <h1 style={EBGaramond.style} className="section-title">Meet Our Team</h1>
       <div className="px-4 md:px-8 lg:grid lg:grid-cols-3 lg:gap-4">
-        <div className="flex flex-wrap justify-center pb-16 w-full px-16 sm:px-48 md:px-60 lg:px-0">
+        <div className="flex flex-wrap justify-center pb-16 w-full px-16 sm:px-48 md:px-60 lg:px-0 lg:pb-8">
           <Image
             // className="w-max"
             width={250}
@@ -17,7 +17,7 @@ export default function Team() {
             priority={true}
           />
           <div className="whitespace-nowrap">
-            <h2 className="font-bold text-xl pt-4 text-center ">
+            <h2 className="font-bold text-xl pt-4 text-center">
               Dongxing &#40;George&#41; Wang
             </h2>
             <p className="text-sm ">
@@ -29,7 +29,7 @@ export default function Team() {
             </p>
           </div>
         </div>
-        <div className="leading-7 md:col-span-2 ">
+        <div className="leading-7 md:col-span-2">
           <p className="pb-6">
             George is the principal person of the firm. He was admitted as a
             barrister and solicitor of the High Court of New Zealand in 2007.
@@ -50,7 +50,7 @@ export default function Team() {
             able to provide clients with low cost and high quality solutions.
           </p>
         </div>
-        <div className="pb-10 lg:col-span-3 lg:text-center lg:pt-4">
+        <div className="pb-10 lg:col-span-3 lg:text-center">
           <h3 className="font-bold text-xl pb-2">Qualifications</h3>
           <ul className="text-sm md:text-base">
             <li>
