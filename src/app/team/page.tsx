f5c1d7f008646bcceffd9cfb8770d8e9ada0c0df
Ian Chua <ianchuajl@hotmail.com>
2025-04-30 16:51:46 +1200

    minor styling change for the our team page

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -2,15 +2,16 @@ import Image from 'next/image'
 import george from 'public/george-wang.jpg'
 import { EBGaramond } from '../fonts/font'
 
-
 export default function Team() {
   return (
     <section className="section">
-      <h1 style={EBGaramond.style} className="section-title">Meet Our Team</h1>
+      <h1 style={EBGaramond.style} className="section-title">
+        Meet Our Team
+      </h1>
       <div className="px-4 md:px-8 lg:grid lg:grid-cols-3 lg:gap-4">
         <div className="flex flex-wrap justify-center pb-16 w-full px-16 sm:px-48 md:px-60 lg:px-0 lg:pb-8">
           <Image
-            // className="w-max"
+            className="sm:h-[280px]"
             width={250}
             src={george}
             alt="Profile picture of George Wang"
@@ -30,27 +31,44 @@ export default function Team() {
           </div>
         </div>
         <div className="leading-7 md:col-span-2">
-          <p className="pb-6">
-            George is the principal person of the firm. He was admitted as a
-            barrister and solicitor of the High Court of New Zealand in 2007.
-          </p>
-          <p className="pb-6">
-            George has worked in two respected New Zealand law firms for three
-            and half years before he set up this firm. He is always indebted to
-            his former employers for the knowledge and experience he has got
-            from them. He also has seven years of work experience in Commerce in
-            China before he emigrated to New Zealand in 2003.
-          </p>
-          <p className="pb-10">
-            George is experienced in Property, Commercial, Contract, Matrimonial
-            Property, Employment and Immigration Law. George&#39;s ability in
-            legal research and analysis, enables him to apply the right law on a
-            case by case basis. In the field of civil litigation, George has
-            cooperated with some trustworthy and reputable barristers and is
-            able to provide clients with low cost and high quality solutions.
-          </p>
+          <div>
+            <h2 className="font-bold text-2xl pb-2">About</h2>
+            <p className="pb-4">
+              George is the principal person of the firm. He was admitted as a
+              barrister and solicitor of the High Court of New Zealand in 2007.
+            </p>
+            <p className="pb-4">
+              George has worked in two respected New Zealand law firms for three
+              and half years before he set up this firm. He is always indebted
+              to his former employers for the knowledge and experience he has
+              got from them. He also has seven years of work experience in
+              Commerce in China before he emigrated to New Zealand in 2003.
+            </p>
+            <p className="pb-8">
+              George is experienced in Property, Commercial, Contract,
+              Matrimonial Property, Employment and Immigration Law. George&#39;s
+              ability in legal research and analysis, enables him to apply the
+              right law on a case by case basis. In the field of civil
+              litigation, George has cooperated with some trustworthy and
+              reputable barristers and is able to provide clients with low cost
+              and high quality solutions.
+            </p>
+          </div>
+          <div className="pb-10 lg:col-span-3 lg:text-left">
+            <h3 className="font-bold text-xl pb-1">Qualifications</h3>
+            <ul className="text-sm md:text-base">
+              <li>
+                Bachelor of Laws &#40;Hons&#41; at{' '}
+                <span className="whitespace-nowrap ">
+                  University of Auckland
+                </span>
+              </li>
+              <li>Bachelor of Engineering</li>
+              <li>Master of Philosophy</li>
+            </ul>
+          </div>
         </div>
-        <div className="pb-10 lg:col-span-3 lg:text-center">
+        {/* <div className="pb-10 lg:col-span-3 lg:text-center">
           <h3 className="font-bold text-xl pb-2">Qualifications</h3>
           <ul className="text-sm md:text-base">
             <li>
@@ -60,7 +78,7 @@ export default function Team() {
             <li>Bachelor of Engineering</li>
             <li>Master of Philosophy</li>
           </ul>
-        </div>
+        </div> */}
       </div>
     </section>
   )
